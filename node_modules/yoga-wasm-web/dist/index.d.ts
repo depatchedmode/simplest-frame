ae8b91f5cacd679e032338a32fc52a695c1f1508
ryanbetts <84613835+depatchedmode@users.noreply.github.com>
2024-01-28 15:34:09 -0800

    Create .gitignore

--- node_modules/yoga-wasm-web/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Yoga } from "./wrapAsm.js";
-
-export * from "./generated/YGEnums.js";
-export * from "./wrapAsm.js";
-
-export default function (wasm: BufferSource | WebAssembly.Module): Promise<Yoga>;
-export function initStreaming(response: Response | PromiseLike<Response>): Promise<Yoga>;
