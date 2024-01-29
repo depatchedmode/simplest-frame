68a57d194198cce1a83851bb0d3643b8aae3492d
ryanbetts <84613835+depatchedmode@users.noreply.github.com>
2024-01-28 20:47:31 -0800

    feature: netlify serverless refactor

---
+++ api/index.js
@@ -0,0 +1,70 @@
+import { getStore } from "@netlify/blobs";
+import { URLSearchParams } from 'url';
+
+
+export default async (req, context) => {
+    const store = getStore('frameState');
+    let count = await store.get('count');
+    
+    const host = process.env.URL;
+
+    if (req.method === 'POST') {
+        let data;
+        if (req.headers['content-type'] === 'application/json') {
+            // Parse JSON body
+            data = JSON.parse(req.body);
+        } else if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+            // Parse URL-encoded body
+            data = Object.fromEntries(new URLSearchParams(req.body));
+        }
+        await store.set('count', (parseInt(count)+1).toString());
+
+    }
+
+    const imagePath = `${host}/og-image?count=${count}`;
+
+    const html = `
+        <!doctype html>
+        <html>
+        <head>
+            <style>
+                figure {
+                    display: inline-block;
+                    margin: 0;
+                    max-width: 100%;
+                }
+                img {
+                    max-width: 100%;
+                    border: 4px inset black;
+                }
+            </style>
+            <meta property="og:image" content="${imagePath}" />
+            <meta property="fc:frame" content="vNext" />
+            <meta property="fc:frame:image" content="${imagePath}" />
+            <meta property="fc:frame:button:1" content="Frame me!" />
+            <title>Simplest Frame</title>
+        </head>
+        <body>
+            <h1>The Simplest Frame</h1>
+            <figure>
+            <img width="600" src="${imagePath}" />
+            </figure>
+            <!-- Form for POST request -->
+            <form action="/" method="post">
+                <input type="submit" value="Frame me!" /> ${count}
+            </form>
+        </body>
+        </html>
+    `
+    
+    return new Response(html, 
+        {
+            status: 200,
+            headers: { 'Content-Type': 'text/html' },
+        }
+    );
+}
+
+export const config = {
+    path: "/"
+};
---
+++ api/og-image.js
@@ -0,0 +1,49 @@
+import satori from "satori";
+import sharp from "sharp";
+import { html } from "satori-html";
+
+export default async (req, context) => {
+    const url = new URL(req.url);
+    const count = url.searchParams.get('count') || 0;
+    console.log(url);
+
+    const host = process.env.URL;
+    const htmlResponse = await fetch(`${host}/frame?count=${count}`);
+    const markup = await htmlResponse.text();
+
+    const font = {
+        fileName: 'Redaction-Regular.otf',
+        cssName: 'Redaction'
+    };
+    const fontResponse = await fetch(`${host}/fonts/${font.fileName}`);
+    const fontData = await fontResponse.arrayBuffer();
+
+    const svg = await satori(
+    html(markup), 
+    {
+        width: 1200,
+        height: 800,
+        fonts: [
+            {
+                name: font.cssName,
+                data: fontData,
+                weight: 400,
+                style: "normal",
+            },
+        ],
+    });
+    const svgBuffer = Buffer.from(svg);
+    const png = sharp(svgBuffer).png();
+    const response = await png.toBuffer();
+
+    return new Response(response,
+        {
+            status: 200,
+            headers: { 'Content-Type': 'image/png' }
+        }
+    );
+}
+
+export const config = {
+    path: "/og-image"
+};
